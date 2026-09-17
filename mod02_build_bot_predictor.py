b07149c2e518607a195407a7db288c4f28275beb
maggiepotts <mgp23012@uconn.edu>
2026-09-17 14:14:10 -0400

    n_estimators=100000 took too long to run so I'm changing it to 10000

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -11,7 +11,7 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.1,
-        n_estimators=100000,
+        n_estimators=10000,
         max_depth=1,
         subsample=1,
         min_samples_leaf=1,
