b2d0966e7ccfcb3f509562d809f24ad444a7bf0f
maggiepotts <mgp23012@uconn.edu>
2026-09-17 10:57:54 -0400

    increasing min_samples_leaf

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
+        learning_rate=0.2,
         n_estimators=10000,
-        max_depth=5,
-        subsample=5,
-        min_samples_leaf=1,
+        max_depth=1,
+        subsample=1,
+        min_samples_leaf=5,
         random_state=seed
     )
     model.fit(X, y)
