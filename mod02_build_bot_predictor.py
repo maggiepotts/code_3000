dc182a799dd32a2b68203364e8c64f30f185017c
maggiepotts <mgp23012@uconn.edu>
2026-09-17 13:27:07 -0400

    smaller depth subsample and min leaf

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -12,9 +12,9 @@ def train_model(X, y, seed=seed):
     model = GradientBoostingClassifier(
         learning_rate=0.1,
         n_estimators=10000,
-        max_depth=1,
+        max_depth=0.5,
         subsample=0.5,
-        min_samples_leaf=5,
+        min_samples_leaf=0.5,
         random_state=seed
     )
     model.fit(X, y)
